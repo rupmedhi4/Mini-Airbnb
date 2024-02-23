f043691cb81a1cf5b3cc38fe0a3f84540d2a121c
Rup medhi <rupmmedhi@gmail.com>
2024-02-23 11:17:14 +0530

    listing model

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const Listing = require('./modals/listing')
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust'
+
+app.listen(4000,()=>{
+    console.log('server is listening to port 4000');
+})
 
 main().then(()=>{
     console.log('connected to DB');
@@ -15,28 +20,36 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-
 app.get('/',(req,res)=>{
     res.send('hi i am root')
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(4000,()=>{
-    console.log('server is listening to port 4000');
+app.get('/testListing', async (req,res)=>{
+    let sampleListing = new Listing({
+        title :"my home",
+        description : " this is my home",
+        price : 1200,
+     
+        country : 'india'
+    })
+    await sampleListing.save()
+    console.log('sample was save');
+    res.send('successful testing')
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ modals/listing.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+const listingSchema = new Schema({
+    title : {
+        type :String,
+        required: true
+    },
+    description : String,
+    image :{
+        type : String,
+        default : "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg",
+        set : (v)=> v === ""? "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg" : v,
+    },
+    location : String,
+    price : Number,
+    country : String
+})
+
+const Listing = mongoose.model('Listing', listingSchema)
+
+module.exports = Listing
