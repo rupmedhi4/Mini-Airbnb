3b8353fd7bd78ba6216381c0a70da5df8b68b742
Rup medhi <rupmmedhi@gmail.com>
2024-02-27 18:51:24 +0530

    footer and navbar

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose')
 const Listing = require('./modals/listing')
 const path = require('path')
 var methodOverride = require('method-override')
+const ejsMate = require('ejs-mate')
 
 
-
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust'
+const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 
 app.listen(4000,()=>{
     console.log('server is listening to port 4000');
@@ -26,9 +26,12 @@ async function main() {
 
 
 app.set('view engine','ejs')
-app.set('views', path.join(__dirname,'views'))
+app.set('views', path.join(
+    __dirname,'views'))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,'/public')))
 
 
 
